1239ef1c808f3f93e8a5e82b7ae1f0fed0b7afc5
javialvarezdrive <153924249+javialvarezdrive@users.noreply.github.com>
2025-02-26 09:09:05 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,16 @@
+# config.py
 import os
 from dotenv import load_dotenv
 
-# Cargar variables de entorno desde el archivo .env (si existe)
-load_dotenv()
+load_dotenv()  # Esto cargará variables del archivo .env si lo tienes localmente
 
-# Configuración de Supabase usando los valores proporcionados como valor por defecto
-SUPABASE_URL = os.getenv("SUPABASE_URL", "https://piahnkbbkxtbvmvzxtqa.supabase.co")
-SUPABASE_KEY = os.getenv("SUPABASE_KEY", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBpYWhua2Jia3h0YnZtdnp4dHFhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA1NTQ2NzcsImV4cCI6MjA1NjEzMDY3N30.oOjENjB2bUileOHelp138Gg6l2JDzP7YuuNHArhapa8")
-
-# Configuración de la aplicación
+# Si estamos en Streamlit Cloud usaremos st.secrets, de lo contrario usaremos .env o valores por defecto
+try:
+    import streamlit as st
+    SUPABASE_URL = st.secrets["supabase"]["SUPABASE_URL"]
+    SUPABASE_KEY = st.secrets["supabase"]["SUPABASE_KEY"]
+except Exception as e:
+    SUPABASE_URL = os.getenv("SUPABASE_URL", "https://piahnkbbkxtbvmvzxtqa.supabase.co")
+    SUPABASE_KEY = os.getenv("SUPABASE_KEY", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBpYWhua2Jia3h0YnZtdnp4dHFhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDA1NTQ2NzcsImV4cCI6MjA1NjEzMDY3N30.oOjENjB2bUileOHelp138Gg6l2JDzP7YuuNHArhapa8")
+    
 APP_NAME = "Gestión de Gimnasio"
